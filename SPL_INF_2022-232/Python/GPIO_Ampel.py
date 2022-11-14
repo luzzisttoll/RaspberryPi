e2cc0c41bf55ff92362f2666d8d318d2cdb7ef48
DevGnomino <damianopezze@gmail.com>
2022-11-14 09:03:54 +0100

    ampel fertig

--- SPL_INF_2022-232/Python/GPIO_Ampel.py
+++ SPL_INF_2022-232/Python/GPIO_Ampel.py
@@ -1,23 +1,22 @@
+from tkinter import E
 import RPi.GPIO as GPIO
 import time
+
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(3,GPIO.OUT)
 GPIO.setup(5,GPIO.OUT)
 GPIO.setup(7,GPIO.OUT)
-i = 0
-while True:
-    i+=1
-    GPIO.output(3,False)
-    GPIO.output(5,False)
-    GPIO.output(7,False)
-    if i == 1:
-        GPIO.output(3,True)
 
-    elif i == 2:
-        GPIO.output(5,True)
-    
-    elif i == 3:
-        GPIO.output(7,True)
+pin = [3, 5, 7]
+i = 0
+
+while True:
+    input("Press enter")
+    GPIO.output(3, False)
+    GPIO.output(5, False)
+    GPIO.output(7, False)
+    GPIO.output(pin[i], True)
+    i+=1
     if i > 2:
         i = 0
